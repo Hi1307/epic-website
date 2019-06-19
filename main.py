07d2ae6dacaaace6eb1a0467344f2dc526738e1e
XlightNtrEnx <xyf.oco@gmail.com>
2019-06-19 12:05:17 +0800

    EWQJIOEJWQOIE

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from google.appengine.api import urlfetch
 import json
 import os
 import jinja2
-from accounts import Account
+import accounts
 
 jinja_current_dir = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -13,13 +13,14 @@ jinja_current_dir = jinja2.Environment(
 
 class WelcomeHandler(webapp2.RequestHandler):
     def get(self):
+        login_url = users.create_login_url("/")
         user = users.get_current_user()
-        if user:
-            pass 
-        else:
-            pass
+        email = user.nickname()
+        dipshit = accounts.Account(email = email)
+        dipshit.put()
+        dictionary = {"login_url": login_url}
         template = jinja_current_dir.get_template("templates/welcome.html")
-        self.response.write(template.render())
+        self.response.write(template.render(dictionary))
 
 class ResultsHandler(webapp2.RequestHandler):
     def get(self):
@@ -32,7 +33,20 @@ class ResultsHandler(webapp2.RequestHandler):
                       }
         self.response.write(template.render(dictionary))
 
+class datastorerHandler(webapp2.RequestHandler):
+    def post(self):
+        favourite_bus_stop_no = self.request.get("favourite_bus_stop_no")
+        user = users.get_current_user()
+        email = user.nickname()
+        Account_query = accounts.Account.query().filter(accounts.Account.email == email)
+        Account = Account_query.fetch()
+        Account[2] = favourite_bus_stop_no
+        dictionary = {"favourite_bus_stop_no": favourite_bus_stop_no,}
+        template = jinja_current_dir.get_template("templates/data_storer.html")
+        self.response.write(template.render(dictionary))
+
 app = webapp2.WSGIApplication([
     ('/results', ResultsHandler),
     ('/', WelcomeHandler),
+    ('/data_storer', datastorerHandler)
 ], debug=True)
